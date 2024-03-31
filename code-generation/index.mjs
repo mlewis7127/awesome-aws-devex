b09014dc9d19ab8df5234dac063234375717604a
Matt Lewis <matt.lewis@ibm.com>
2024-03-31 11:05:00 +0100

    Add code generation samples

---
+++ code-generation/index.mjs
@@ -0,0 +1,6 @@
+/**
+ * AWS Lambda handler
+ *  
+ * Exports a single function that takes the "operand" property from the event
+ * input, squares it, and returns it.
+ */
---
+++ code-generation/index.py
@@ -0,0 +1,6 @@
+import json
+
+'''
+lambda_handler function that accepts user input of a first and last name,
+and returns a message that contains data from the event it received as input
+'''
